3be02081ce8398b4ec8ea2815b84c6b853a6b24c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-06-02 16:16:02 -0300

    funcoes nativas de string parte 2

--- modulo-2/aula-10/index.php
+++ modulo-2/aula-10/index.php
@@ -1,6 +1,7 @@
 <?php 
 
-$nome = 'Gustavo Alves da Silva';
+$nome = 'gustavo alves da silva';
+$numero = 14343.54;
 
 // remove espaços no começo e no fim da string
 # echo trim($nome);
@@ -21,3 +22,20 @@ $nome = 'Gustavo Alves da Silva';
 # echo substr($nome, 7);
 // segundo parâmetro é a quantidade de caracteres que eu quero pegar
 # echo substr($nome, 7, 6);
+
+// procura um caractere ou palavra dentro de outra e retorna a posição onde se encontra o termo procurado
+# echo strpos($nome, 'Alves');
+// quando não tem retorna false 
+# echo (strpos($nome, 'Ferreira')) ? strpos($nome, 'Ferreira') : 'Não tem';
+
+// transforma a primeira letra da string em maiúscula
+# echo ucfirst($nome);
+
+// transforma a primeira letra de cada palavra da string em maiúscula
+# echo ucwords($nome);
+
+// transforma cada palavra de uma string em um item de um array
+# print_r(explode(' ', $nome));
+
+// formata números
+echo 'R$ '.number_format($numero, 2, ',', '.');
